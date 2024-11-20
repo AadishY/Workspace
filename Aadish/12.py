5bcc8d6e8167cfd65fe913d6b61458c68939ca18
Aadish <101470733+AadishY@users.noreply.github.com>
2024-11-20 21:21:14 +0530

    Delete Aadish directory

--- Aadish/12.py
+++
@@ -1,8 +0,0 @@
-D1 = eval(input("Enter a dictionary D1: "))
-print("D1 =", D1) 
-
-D2 = {}
-for i in D1:
-      num = sum(D1[i])
-      D2[i] = num
-print(D2)
--- Aadish/8.py
+++
@@ -1,5 +0,0 @@
-x = { "k1" : "v1" , "k2" : "v2", "k3" : "v3"}
-inverted_x = {}
-for i in x :
-    inverted_x[x[i]] = i
-print(inverted_x)
